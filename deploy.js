f929780b22d477c8bf0fbc2e516917f52163631b
peter_alfie <alfie1104@gmail.com>
2022-11-24 08:51:20 +0900

    Wallet provider setup

--- deploy.js
+++ deploy.js
@@ -1,1 +1,12 @@
 // deploy code will go here
+require("dotenv").config(); //environment variable
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
+const { interface, bytecode } = require("./compile");
+
+const provider = new HDWalletProvider(
+  process.env.ACCOUNT_MNEMONIC,
+  process.env.GORLI_URL
+);
+
+const web3 = new Web3(provider);
